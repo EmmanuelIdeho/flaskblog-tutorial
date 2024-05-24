f2ddc41537153c7b4036c56f254a643bad22c9c6
EmmanuelIdeho <56139812+EmmanuelIdeho@users.noreply.github.com>
2024-05-24 17:02:57 -0400

    Completed tutorial #1

--- website/auth.py
+++ website/auth.py
@@ -1,17 +1,17 @@
 #auth.py will have all the routes concerning authentication:
 #login, sign-up, sign-out, etc.
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
 
 auth = Blueprint("auth", __name__)
 
 @auth.route("/login")
 def login():
-    return "Login"
+    return render_template("login.html")
 
 @auth.route("/signup")
 def signup():
-    return "Sign-up"
+    return render_template("signup.html")
 
-@auth.route("/signout")
-def signout():
-    return "Sign-out"
+@auth.route("/logout")
+def logout():
+    return redirect(url_for("views.home"))

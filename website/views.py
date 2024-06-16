f59a2a693faf54892b784ddf69861b7baf38ac43
EmmanuelIdeho <56139812+EmmanuelIdeho@users.noreply.github.com>
2024-06-16 00:22:05 -0400

    Creating and Deleting Posts

--- website/views.py
+++ website/views.py
@@ -2,7 +2,7 @@
 #home page, profile page, creative post page, etc.
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_required, current_user
-from .models import Post
+from .models import Post, User
 from . import db
 
 views = Blueprint("views", __name__)
@@ -30,3 +30,30 @@ def create_post():
             return redirect(url_for('views.home'))
 
     return render_template('create_post.html', user=current_user)
+
+@views.route("/delete-post/<id>")
+@login_required
+def delete_post(id):
+    post = Post.query.filter_by(id=id).first()
+    if not post:
+        flash("post does not exit", category='error')
+    elif current_user.id != post.author:
+        flash('you do not have permission to delete this post', category='error')
+    else:
+        db.session.delete(post)
+        db.session.commit()
+        flash('post successfully deleted', category='success')
+    
+    return redirect(url_for('views.home'))
+
+@views.route("/posts/<username>")
+@login_required
+def posts(username):
+    user = User.query.filter_by(username=username).first()
+
+    if not user:
+        flash('No user with that username exists.', category='error')
+        return redirect(url_for('views.home'))
+    
+    posts = Post.query.filter_by(author=user.id).all()
+    return render_template("posts.html", user=current_user, posts=posts, username=username)

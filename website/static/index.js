744ec10ddb9396106fd30a4f4b92eb0bb09760e5
EmmanuelIdeho <56139812+EmmanuelIdeho@users.noreply.github.com>
2024-06-20 11:42:34 -0400

    Started setting up 'like' feature using JavaScript

---
+++ website/static/index.js
@@ -0,0 +1,6 @@
+function like(post_id){
+    const likeCount = document.getElementById(`likes-count-${postId}`)
+    const likeButton = document.getElementById(`like-button-${postId}`)
+
+    console.log(likeCount.value)
+}
